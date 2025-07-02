3afdf269b98bd7cf6acf674b5e69aeb26f2dc891
adbean <adbeanx@outlook.com>
2025-07-02 00:06:20 -0700

    fix: auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,6 +6,7 @@ import bcrypt from "bcryptjs";
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
